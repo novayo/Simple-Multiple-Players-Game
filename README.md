65a0e40c166d20872270e8b66a7a1920167454db
施崇祐 <f14051172@gs.ncku.edu.tw>
2020-11-06 21:27:27 +0800

    Finish Connect to Server ; Create game

---
+++ README.md
@@ -0,0 +1,54 @@
+# 1. 多人連線遊戲 (未完成)
+###### tags: `Side Projects`
+
+## 遊戲架構
+1. 開發語言：Nodejs
+2. Agenda
+    * 連接到server
+    * 開啟新遊戲（不同的game id）
+    * 加入新遊戲（加入game id）
+    * 設計遊戲
+    * broadcast（更新所有使用者的狀態）
+
+## 前端
+### 連接到server
+1. 創建client，用express，並寫一個簡單的html當作client
+    ```htmlembedded=
+    <script>
+        // 創建對8080的 socket
+        let wsServer = new WebSocket("ws://localhost:8080");
+
+        // 接收server的封包
+        wsServer.onmessage = message => {
+            const result = JSON.parse(message.data);
+        }
+    </script>
+    ```
+### 開啟新遊戲（不同的game id）
+1. 創建按鈕，按下後傳送create封包給server(含clientId)
+2. 監聽server傳的 create 封包(含game資訊)，並將資訊存起來
+
+## 後端
+### 連接到server
+1. 建立http，建立httpServer
+2. 建立websocketServer去管理httpServer
+3. 當建立連接時，給予client一個id，傳送connect封包給client
+    ```javascript=
+    const payload = {
+        "method": "connect",
+        "clientId": clientId,
+    };
+    ```
+
+### 開啟新遊戲（不同的game id）
+1. 監聽client傳的 create 封包，並去建立一個game去存gameId與球個數
+2. 由於所有的連接都會到這個server，所以要用clientId去做區隔，並把此clientId的connection給存起來，之後找到對應的clientId.connection並傳回此create封包
+
+## 我學到了什麼？
+
+## 遇到的困難
+
+## Reference
+### [學習影片](https://www.youtube.com/watch?v=cXxEiWudIUY&t=59m12s)
+### [websocket 套件]( https://github.com/websockets/ws/blob/HEAD/doc/ws.md)
+### [不重複的guid](https://stackoverflow.com/posts/44996682/revisions)
---
+++ index.js
@@ -0,0 +1,73 @@
+// 建立client 執行 index.html
+const app = require("express")();
+app.listen(8081, () => console.log("Client Listening on 8081"));
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+
+// 建立http協議
+const http = require("http");
+const httpServer = http.createServer();
+httpServer.listen(8080, () => console.log("Server Listening on 8080"));
+
+// 建立socket server去做管理
+const clients = {};
+const games = {};
+const websocketServer = require("websocket").server;
+const wsServer = new websocketServer({
+    "httpServer": httpServer,
+});
+
+// request is the http GET request sent by the client.
+wsServer.on("request", request => {
+    // client與server之間的建立連接
+    // 可以設定要接受什麼條件的連接（這裡是無條件）
+    const connection = request.accept(null, request.origin);
+
+    // 建立連接時，為什麼沒用？
+    connection.on("open", () => console.log("Connect !"));
+
+    // 斷開連接時
+    connection.on("close", () => console.log("Close !"));
+
+    // 當client傳封包到server時
+    connection.on("message", message => {
+        const result = JSON.parse(message.utf8Data);
+
+        // create Router
+        // 由於所有的連接都會到這個server，所以要用clientId去做區隔，並把此id的connection給存起來
+        if (result.method === "create") {
+            const clientId = result.clientId;
+            const gameId = guid();
+            games[gameId] = {
+                "id": gameId,
+                "balls": 20,
+            }
+
+            const payload = {
+                "method": "create",
+                "game": games[gameId],
+            }
+
+            const client_connection = clients[clientId].connection;
+            client_connection.send(JSON.stringify(payload));
+        }
+    });
+
+    // connection Router
+    const clientId = guid();
+    clients[clientId] = {
+        "connection": connection,
+    };
+
+    const payload = {
+        "method": "connect",
+        "clientId": clientId,
+    };
+
+    connection.send(JSON.stringify(payload));
+});
+
+// 建立 client uid
+const guid = () => {
+    const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    return `${s4() + s4()}-${s4()}-${s4()}-${s4()}-${s4() + s4() + s4()}`;
+}
